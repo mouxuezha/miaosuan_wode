c6f42696501f8332a5e88a287968f0da4229641b
mouxuezha <424180922@qq.com>
2024-01-15 19:49:01 +0800

    modified:   agent_guize.py 	modified:   auto_run.py 	.gitignore

--- agent_guize.py
+++ agent_guize.py
@@ -1,10 +1,13 @@
 # this is wode agent for miaosuan, define some layers.
+import os 
 import sys 
+import json
 sys.path.append("/home/vboxuser/Desktop/miaosuan_code/sdk")
-from ai.agent import Agent
+from ai.agent import Agent, ActionType, BopType, MoveType
 from ai.base_agent import BaseAgent
-from map import Map
-class agent_guize(BaseAgent):
+from ai.map import Map
+
+class agent_guize(Agent):
     def __init__(self):
         self.scenario = None
         self.color = None
@@ -25,11 +28,19 @@ class agent_guize(BaseAgent):
         self.history = None
         # abstract_state is useful
         self.absract_state = {} 
+        # ["move_and_attack", only for tank, 
+        # "move_and_attack2", for other units
+        # "follow_and_guard",  
+        # "hide_and_alert",  for most dongxi
+        # "charge_and_xiache", for infantry and che
+        # ]
 
         self.act = [] # list to save all commands generated.
 
         self.state = {}
         self.detected_state = {} 
+
+        self.num = 0 
 
     def setup(self, setup_info):
         self.scenario = setup_info["scenario"]
@@ -69,24 +80,120 @@ class agent_guize(BaseAgent):
             setup_info["see_data"]
         )  # use 'Map' class as a tool
         self.map_data = self.map.get_map_data()
+    def reset(self):
+        self.scenario = None
+        self.color = None
+        self.priority = None
+        self.observation = None
+        self.map = None
+        self.scenario_info = None
+        self.map_data = None
+
+        self.num = 0 
 
     def get_detected_state(self,state):
         # it is assumed that only my state passed here.
-        self.detected_state = {} 
+        # 0106,it is said that detected enemy also included in my state.
+        self.detected_state = []
         units = state["operators"]
         for unit in units:
             detected_IDs = unit["see_enemy_bop_ids"]
             for detected_ID in detected_IDs:
-                detected_state_single = {} 
+                detected_state_single = self.select_by_type(units,key="obj_id", value=detected_ID)
                 # xxh 1226 legacy issues: how to get the state of emeny without the whole state?
-                self.detected_state[detected_ID] = detected_state_single
+                self.detected_state = self.detected_state + detected_state_single
         return self.detected_state
-
-    def _move_action(self,attacker_ID, target_pos):
+    
+    def get_move_type(self, bop):
+        """Get appropriate move type for a bop."""
+        bop_type = bop["type"]
+        if bop_type == BopType.Vehicle:
+            if bop["move_state"] == MoveType.March:
+                move_type = MoveType.March
+            else:
+                move_type = MoveType.Maneuver
+        elif bop_type == BopType.Infantry:
+            move_type = MoveType.Walk
+        else:
+            move_type = MoveType.Fly
+        return move_type
+    def get_scenario_info(self, scenario: int):
+        SCENARIO_INFO_PATH = os.path.join(
+            os.path.dirname(__file__), f"scenario_{scenario}.json"
+        )
+        with open(SCENARIO_INFO_PATH, encoding="utf8") as f:
+            self.scenario_info = json.load(f)
+    def get_bop(self, obj_id):
+        """Get bop in my observation based on its id."""
+        for bop in self.observation["operators"]:
+            if obj_id == bop["obj_id"]:
+                return bop
+    def select_by_type(self,units,key="obj_id",value=0):
+        # a common tool, for selecting units according to different keys.
+        units_selected = [] 
+        for unit in units:
+            value_candidate = unit[key]
+            if type(value_candidate) == str:
+                if value in value_candidate:
+                    # so this one is selected
+                    units_selected.append(unit)
+            else:
+                if value==value_candidate:
+                    # so this one is selected
+                    units_selected.append(unit)
         
-        pass
+        return units_selected
+
+    def get_prior_list(self,unit):
+        # different weapons have common CD, so one prior list for one unit seems enough.
+        # unit is a operator, a dict.
+        unit_type = unit["sub_type"]
+        prior_list = [] 
+        # -1 for everything else, use it carefully 
+        if unit_type == 0: # tank 
+            prior_list = [0, 1, 4]
+        elif unit_type == 1: # fighting vechile
+            prior_list = [0,1,4,-1]
+        elif unit_type == 2:  # infantry
+            prior_list = [-1] # infantry attack everything
+        elif unit_type == 3: # pao 
+            prior_list = [-1] # pao did not use prior list 
+        elif unit_type == 4: # unmanned che 
+            prior_list = [5,7,6,8,2,0,1,-1] # the only airdefender 
+        elif unit_type == 5: # UAV 
+            pass 
+        elif unit_type == 6: # helicopter
+            pass
+        elif unit_type == 7: # xunfei missile
+            pass 
+        elif unit_type == 8: # transport helicopter 
+            pass 
+        else:
+            pass 
+
+        return prior_list
+
+    # basic AI interface.
+    def _move_action(self,attacker_ID, target_pos):
+        bop = self.get_bop(attacker_ID)
+        move_type = self.get_move_type(bop)
+        route = self.map.gen_move_route(bop["cur_hex"], target_pos, move_type)
+        
+        action_move =  {
+                "actor": self.seat,
+                "obj_id": attacker_ID,
+                "type": ActionType.Move,
+                "move_path": route,
+            }
+        self.act.append(action_move)
+        return self.act
 
     def _fire_action(self,attacker_ID, target_ID, weapon_type):
+        # check if th fire action is valid.
+
+        # if so, shoot.
+
+        # if not, nothing happen.
         pass
 
 
@@ -103,6 +210,13 @@ class agent_guize(BaseAgent):
 
     # then step
     def step(self, observation: dict):
+
+        self.num = self.num + 1 
+        if self.num == 114514:
+            print("Debug, moving")
+        else:
+            if self.num%100==99:
+                print("Debug, self.num = "+str(self.num))
         self.observation = observation
         self.team_info = observation["role_and_grouping_info"]
         self.controllable_ops = observation["role_and_grouping_info"][self.seat][
@@ -120,9 +234,12 @@ class agent_guize(BaseAgent):
         # update the actions
         self.Gostep_absract_state()
 
-        pass
+        return self.act
 
     def step0(self):
         # this is the first one for learning the guize of miaosuan, 1226 xxh.
-        self.gen_move(self.units[0])
+        unit0 = self.get_bop(0)
+        pos_0 = unit0["cur_hex"]
+        target_pos = pos_0 + 3
+        self._move_action(unit0["obj_id"],target_pos)
         pass
